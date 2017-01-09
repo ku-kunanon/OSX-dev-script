8d2bbe3ad70c6f94de0dd87241526967f798ff7e
Feelingfree <rattapoom_m@cmu.ac.th>
2017-01-09 12:17:18 +0700

    add install java jre

--- Install.sh
+++ Install.sh
@@ -3,7 +3,7 @@
 # @Author: feelingfree
 # @Date:   2016-08-18 16:16:53
 # @Last Modified by:   Feelingfree
-# @Last Modified time: 2017-01-09 11:57:29
+# @Last Modified time: 2017-01-09 12:14:06
 
 # install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -14,6 +14,9 @@ brew install python3
 # remove and create link python3
 brew unlink python3
 brew link python3
+
+# install java
+brew cask install java
 
 # install golang
 brew install go
